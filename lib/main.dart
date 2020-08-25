f5fcd8d69363010bdee5bb6114b1c3a326eb76db
haseeb zafar <Haseebzafar107@gmail.com>
2020-08-25 13:54:47 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,19 @@ SharedPreferences prefs;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   prefs = await SharedPreferences.getInstance();
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
+
+    // statusBarColor is used to set Status bar color in Android devices.
+      statusBarColor: Colors.transparent,
+
+      // To make Status bar icons color white in Android devices.
+      statusBarIconBrightness: Brightness.light,
+
+      // statusBarBrightness is used to set Status bar icon color in iOS.
+      statusBarBrightness: Brightness.light
+    // Here light means dark color Status bar icons.
+
+  ));
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
